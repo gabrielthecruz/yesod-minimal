455a51f7f677672a1739645829bc31ed35b6473c
Gabriel Cruz <gabrielcruz023@users.noreply.github.com>
2022-08-25 20:31:40 -0300

    empty yesod project

---
+++ src/Handler/Home.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+
+getHomeR :: Handler Html
+getHomeR = 
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "homepage")
+        
